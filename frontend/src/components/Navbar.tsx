5e9b43c709bc7d74c5216b07b83c2a4bd78b8e43
Swastik Goswami <cwswastik2005@gmail.com>
2025-07-04 09:36:04 +0530

    feat: switch to material symbols

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,40 +1,40 @@
-import {
-  HomeIcon,
-  TrophyIcon,
-  ChartBarIcon,
-  BookOpenIcon,
-  UserIcon,
-} from "@heroicons/react/24/solid";
 import { Link } from "@tanstack/react-router";
 
 export default function Navbar() {
   return (
-    <nav className="fixed inset-y-0 left-0 w-30 bg-highlight-dark text-white flex flex-col items-center py-4 shadow-lg">
+    <nav className="fixed inset-y-0 left-0 w-30 bg-highlight-dark text-white flex flex-col items-center py-4 shadow-lg z-50">
       <Link to="/" className="mb-16 mt-6">
         <img src="/logo.png" alt="Logo" className="w-14 h-14" />
       </Link>
 
       <div className="flex flex-col items-center gap-12 flex-grow mt-4">
-        <Link to="/" className="text-highlight-light hover:text-white">
-          <HomeIcon className="w-12 h-12" />
+        <Link to="/" className="hover:text-white">
+          <span className="material-symbols-outlined  text-highlight-light">
+            home
+          </span>
         </Link>
-        <Link
-          to="/leaderboard"
-          className="text-highlight-light hover:text-white"
-        >
-          <TrophyIcon className="w-12 h-12" />
+        <Link to="/leaderboard" className="hover:text-white">
+          <span className="material-symbols-outlined  text-highlight-light">
+            emoji_events
+          </span>
         </Link>
-        <Link to="/stats" className="text-highlight-light hover:text-white">
-          <ChartBarIcon className="w-12 h-12" />
+        <Link to="/stats" className="hover:text-white">
+          <span className="material-symbols-outlined text-highlight-light">
+            swords
+          </span>
         </Link>
-        <Link to="/resource" className="text-highlight-light hover:text-white">
-          <BookOpenIcon className="w-12 h-12" />
+        <Link to="/resource" className="hover:text-white">
+          <span className="material-symbols-outlined text-[48px] text-highlight-light">
+            book_ribbon
+          </span>
         </Link>
       </div>
 
       <div className="mb-4">
-        <Link to="/profile" className="text-highlight-light hover:text-white">
-          <UserIcon className="w-12 h-12" />
+        <Link to="/profile" className="hover:text-white">
+          <span className="material-symbols-outlined text-highlight-light">
+            account_circle
+          </span>
         </Link>
       </div>
     </nav>
