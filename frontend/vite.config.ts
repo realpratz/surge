9e100533854eec72b425aa7a22e6b2e28ce4aa6e
Swastik Goswami <cwswastik2005@gmail.com>
2025-07-15 12:38:24 +0530

    fix: hot reload on dev profile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,21 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackRouter } from "@tanstack/router-plugin/vite"
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tanstackRouter({target: 'react', autoCodeSplitting: true,}),
+    tanstackRouter({ target: "react", autoCodeSplitting: true }),
     react(),
     tailwindcss(),
   ],
   server: {
     host: true,
     port: Number(process.env.FRONTEND_PORT) || 5173,
+    watch: {
+      usePolling: true,
+      ignored: ["**/node_modules/**"],
+    },
     hmr: {
-      host: 'localhost', // or your host machine's IP (not 127.0.0.1)
       port: Number(process.env.FRONTEND_PORT) || 5173,
     },
-  }
+  },
 });
