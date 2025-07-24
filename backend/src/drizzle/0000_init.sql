d152acc2b14f3356b453a3a5dbc2ffa7a6da3642
aten2005 <gogo.june2005@gmail.com>
2025-07-24 10:41:39 +0530

    chore(migrations): squashed migrations to 0000_init.sql

--- backend/src/drizzle/0000_init.sql
+++ backend/src/drizzle/0000_init.sql
@@ -1,4 +1,3 @@
-/*
 CREATE TYPE "public"."verdict" AS ENUM('AC', 'WA', 'TLE', 'RE', 'CE', 'other');--> statement-breakpoint
 CREATE TABLE "cf_user_stats" (
 	"id" serial PRIMARY KEY NOT NULL,
@@ -68,20 +67,26 @@ CREATE TABLE "user_contests" (
 	"update_time" timestamp(0)
 );
 --> statement-breakpoint
-ALTER TABLE "_prisma_migrations" DISABLE ROW LEVEL SECURITY;--> statement-breakpoint
-DROP TABLE "_prisma_migrations" CASCADE;--> statement-breakpoint
-ALTER TABLE "User" RENAME TO "users";--> statement-breakpoint
-ALTER TABLE "users" RENAME COLUMN "pfpUrl" TO "pfp_url";--> statement-breakpoint
-ALTER TABLE "users" RENAME COLUMN "cfHandle" TO "cf_handle";--> statement-breakpoint
-ALTER TABLE "users" RENAME COLUMN "cfRating" TO "cf_rating";--> statement-breakpoint
-ALTER TABLE "users" RENAME COLUMN "createdAt" TO "created_at";--> statement-breakpoint
-DROP INDEX "User_cfHandle_key";--> statement-breakpoint
-DROP INDEX "User_email_key";--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "codechef_handle" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "codechef_rating" integer;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "atcoder_handle" text;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "atcoder_rating" integer;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "leetcode_handle" text;--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" text PRIMARY KEY NOT NULL,
+	"email" text NOT NULL,
+	"name" text,
+	"pfp_url" text,
+	"cf_handle" text,
+	"cf_rating" integer,
+	"codechef_handle" text,
+	"codechef_rating" integer,
+	"atcoder_handle" text,
+	"atcoder_rating" integer,
+	"leetcode_handle" text,
+	"created_at" timestamp(3) DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email"),
+	CONSTRAINT "users_cf_handle_unique" UNIQUE("cf_handle"),
+	CONSTRAINT "users_codechef_handle_unique" UNIQUE("codechef_handle"),
+	CONSTRAINT "users_atcoder_handle_unique" UNIQUE("atcoder_handle"),
+	CONSTRAINT "users_leetcode_handle_unique" UNIQUE("leetcode_handle")
+);
+--> statement-breakpoint
 ALTER TABLE "cf_user_stats" ADD CONSTRAINT "cf_user_stats_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "potd" ADD CONSTRAINT "potd_problem_id_problems_id_fk" FOREIGN KEY ("problem_id") REFERENCES "public"."problems"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "potd_solves" ADD CONSTRAINT "potd_solves_potd_id_potd_id_fk" FOREIGN KEY ("potd_id") REFERENCES "public"."potd"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
@@ -92,9 +97,4 @@ ALTER TABLE "submissions" ADD CONSTRAINT "submissions_problem_id_problems_id_fk"
 ALTER TABLE "user_contests" ADD CONSTRAINT "user_contests_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "user_contests" ADD CONSTRAINT "user_contests_contest_id_contests_external_id_fk" FOREIGN KEY ("contest_id") REFERENCES "public"."contests"("external_id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 CREATE UNIQUE INDEX "problems_contest_index" ON "problems" USING btree ("contest_id","index");--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_email_unique" UNIQUE("email");--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_cf_handle_unique" UNIQUE("cf_handle");--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_codechef_handle_unique" UNIQUE("codechef_handle");--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_atcoder_handle_unique" UNIQUE("atcoder_handle");--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_leetcode_handle_unique" UNIQUE("leetcode_handle");
-*/
+CREATE UNIQUE INDEX "user_contest_unique" ON "user_contests" USING btree ("user_id","contest_id");
--- backend/src/drizzle/0002_user_contest_unique.sql
+++
@@ -1,3 +0,0 @@
-/*
-CREATE UNIQUE INDEX "user_contest_unique" ON "user_contests" USING btree ("user_id","contest_id");
-*/
